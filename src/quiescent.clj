c2e4f40d1c06fd7f1ea24abb0c282946848f9aec
Francis Avila <favila@breezeehr.com>
2014-05-07 17:09:38 -0500

    Add macros createMixin, createClass and defclass.

--- src/quiescent.clj
+++ src/quiescent.clj
@@ -21,3 +21,40 @@
 
 (defmacro set-prop! [o propname fn]
   `(let [newo# ~o] (set! (. newo# ~propname) ~fn) newo#))
+
+
+(defmacro createMixin [& proto+specs]
+  ;; TODO: Combine different signatures.
+  (let [[obj-specs proto+specs] (split-with (complement symbol?) proto+specs)
+        {obj-props false obj-meths true} (group-by (comp vector? second)
+                                                   obj-specs)
+        o (gensym)]
+    `(let [~o (~'js-obj)]
+       ~@(for [[pname & pbody] obj-props]
+           `(set! (. ~o ~(symbol (str \- (name pname))))
+                  (do ~@pbody)))
+       ~@(for [[mname margs & mbody] obj-meths]
+           `(set! (. ~o ~(symbol (str \- (name mname))))
+                  (fn ~(subvec margs 1)
+                    (this-as
+                      ~(first margs)
+                      ~@mbody))))
+       ~(if (empty? proto+specs)
+          o
+          `(specify! ~o ~@proto+specs)))))
+
+(defmacro createClass [& specs]
+  `(js/React.createClass (createMixin ~@specs)))
+
+(defmacro defclass [name docstr & specs]
+  (assert (symbol? name) "Must supply a symbol for name (as with def).")
+  (let [[docstr specs] (if (string? docstr)
+                      [docstr specs]
+                      ["" (cons docstr specs)])
+        specs (if (->> (split-with (complement symbol?) specs)
+                       (some #(= % 'displayName)))
+                (cons (list 'displayName name) specs)
+                specs)]
+    `(def ~name ~docstr (createClass ~@specs))))
+
+[]

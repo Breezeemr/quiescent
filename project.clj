e40ef7b10cba0c2807ae29c29f7d9357d2418828
Francis Avila <favila@breezeehr.com>
2015-02-23 17:07:34 -0600

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject breeze-quiescent "0.1.2"
+(defproject breeze-quiescent "0.1.3-SNAPSHOT"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}

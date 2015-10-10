349622bd449d13f398298bfcba7bbb5bd39a0c3a
ttallman <dspiteself@gmail.com>
2015-10-09 23:46:55 -0500

    update deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
                  ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122" :scope "provided"]
                  [cljsjs/react "0.13.3-1"]]
   :source-paths ["src"]
   :release-tasks [["vcs" "assert-committed"]
@@ -21,7 +21,7 @@
   ;; development concerns
   :profiles {:dev {:source-paths ["src" "examples/src"]
                    :resource-paths ["examples/resources"]
-                   :plugins [[lein-cljsbuild "1.0.1"]]
+                   :plugins [[lein-cljsbuild "1.1.0"]]
                    :cljsbuild
                    {:builds
                     [{:source-paths ["src" "examples/src"]
@@ -29,8 +29,8 @@
                       {:output-to "examples/resources/public/main.js"
                        :output-dir "examples/resources/public/build"
                        :optimizations :whitespace
-                       :preamble ["react/react.min.js"]
-                       :externs ["react/externs/react.js"]
+                       ;:preamble ["react/react.min.js"]
+                       ;:externs ["react/externs/react.js"]
                        :pretty-print true
                        :source-map
                        "examples/resources/public/main.js.map"

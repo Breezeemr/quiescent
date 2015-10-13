cd22bf4e68439f2c6f64385ed2701f7b05d28e09
Francis Avila <favila@breezeehr.com>
2015-10-12 23:40:49 -0500

    Add no-overhead macros for React.DOM elements; general refactor of dom.

--- src/quiescent/dom.clj
+++ src/quiescent/dom.clj
@@ -1,24 +1,86 @@
-(ns quiescent.dom)
+(ns quiescent.dom
+  (:refer-clojure :exclude [map meta time])
+  (:require cljs.tagged-literals)
+  (:import (cljs.tagged_literals JSValue)))
 
-(defn tag-definition
-  "Return a form to defining a wrapper function for a ReactJS tag
-  component."
+(defn- tag-fn*
+  "Return a form to defining a wrapper function for a base ReactDOM Element."
   [tag]
-  (let [f (symbol (str "js/React.DOM." (name tag)))]
-    `(defn ~tag [& args#]
-       ~(str "Return a component for ")
-       (let [a# (make-array 0)]
-         (.push a# (quiescent.dom/js-props (first args#)))
-         (doseq [arg# (rest args#)] (.push a# arg#))
-         (.apply ~f nil a#)))))
+  (let [f (with-meta
+            (symbol "js" (str "React.DOM." (name tag)))
+            {:tag 'object})]
+    `(defn ~tag
+       ~(str "Return a `" (name tag) "` ReactElement.")
+       {:tag 'not-native}
+       ([]
+         (~f nil))
+       ([~'attrs]
+         (~f (quiescent.dom/js-props ~'attrs)))
+       ([~'attrs ~'c]
+         (~f (quiescent.dom/js-props ~'attrs) ~'c))
+       ([~'attrs ~'c1 ~'c2]
+         (~f (quiescent.dom/js-props ~'attrs) ~'c1 ~'c2))
+       ([~'attrs ~'c1 ~'c2]
+         (~f (quiescent.dom/js-props ~'attrs) ~'c1 ~'c2))
+       ([~'attrs ~'c1 ~'c2 ~'c3]
+         (~f (quiescent.dom/js-props ~'attrs) ~'c1 ~'c2 ~'c3))
+       ([~'attrs ~'c1 ~'c2 ~'c3 ~'c4]
+         (~f (quiescent.dom/js-props ~'attrs) ~'c1 ~'c2 ~'c3 ~'c4))
+       ([~'attrs ~'c1 ~'c2 ~'c3 ~'c4 ~'c5 & ~'children]
+         (let [a# (cljs.core/make-array 6)]
+           (aset a# 0 (quiescent.dom/js-props ~'attrs))
+           (aset a# 1 ~'c1)
+           (aset a# 2 ~'c2)
+           (aset a# 3 ~'c3)
+           (aset a# 4 ~'c4)
+           (aset a# 5 ~'c5)
+           (reduce #(do (.push %1 %2) %1) a# ~'children)
+           (.apply ~f nil a#))))))
 
-(defmacro define-tags
-  "Macro which expands to a do block which contains a defmacro for
-  each supported HTML and SVG tag. The resulting macros take
+(defn- tag-macro-fn+attrs* [fsym attrs]
+  (list fsym
+    (cond
+      (nil? attrs) nil
+      (instance? JSValue attrs) attrs
+      (map? attrs) (cljs.tagged-literals/read-js attrs)
+      :else (list 'quiescent.dom/js-props attrs))))
+
+(defmacro ^:private tag-macro*
+  "Define a macro which calls a React.DOM factory."
+  [tag]
+  (let [f (symbol "js" (str "React.DOM." (name tag)))]
+    `(defmacro ~tag
+       ~(str "Return a `" (name tag) "` ReactElement.")
+       ([]
+         (list '~f nil))
+       ([~'attrs]
+         (tag-macro-fn+attrs* '~f ~'attrs))
+       ([~'attrs & ~'children]
+         (concat (tag-macro-fn+attrs* '~f ~'attrs) ~'children)))))
+
+(def ReactDOM-elements
+  '[a abbr address area article aside audio b base bdi bdo big blockquote body br
+    button canvas caption cite code col colgroup data datalist dd del details dfn
+    dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5
+    h6 head header hr html i iframe img input ins kbd keygen label legend li link
+    main map mark menu menuitem meta meter nav noscript object ol optgroup option
+    output p param picture pre progress q rp rt ruby s samp script section select
+    small source span strong style sub summary sup table tbody td textarea tfoot th
+    thead time title tr track u ul var video wbr
+
+    circle clipPath defs ellipse g line linearGradient mask path pattern polygon
+    polyline radialGradient rect stop svg text tspan])
+
+(defmacro ^:private define-tag-macros []
+  `(do ~@(clojure.core/map #(list 'tag-macro* %) ReactDOM-elements)))
+
+(define-tag-macros)
+
+(defmacro define-tag-functions
+  "Macro which expands to a do block which contains a defn for
+  each supported HTML and SVG tag. The resulting functions take
   an (optional) properties argument, and any number of child
   arguments. The properties argument may be a Clojure map or a JS
   object."
-  [& tags]
-  (apply list 'do (clojure.core/map tag-definition tags)))
-
-
+  []
+  `(do ~@(clojure.core/map tag-fn* ReactDOM-elements)))
--- src/quiescent/dom.cljs
+++ src/quiescent/dom.cljs
@@ -1,7 +1,8 @@
 (ns quiescent.dom
-  (:refer-clojure :exclude [time map meta])
-  (:require [quiescent :as q])
-  (:require-macros [quiescent.dom :as dm]))
+  (:refer-clojure :exclude [map meta time])
+  (:require-macros [quiescent.dom :as dm])
+  (:require cljsjs.react
+            [goog.object :as gobj]))
 
 (defn js-props
   "Utility function. Takes an object which is (possibly) a
@@ -9,19 +10,15 @@
   to a JavaScript properties object. Otherwise, return the argument
   unchanged."
   [obj]
-  (if (map? obj)
-    (let [o (js-obj)]
-      (doseq [[k v] obj] (aset o (name k) (js-props v)))
-      o)
-    obj))
+  (cond
+    (nil? obj) nil
 
-(dm/define-tags
-  a abbr address area article aside audio b base bdi bdo big blockquote body br
-  button canvas caption cite code col colgroup data datalist dd del details dfn
-  div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6
-  head header hr html i iframe img input ins kbd keygen label legend li link main
-  map mark menu menuitem meta meter nav noscript object ol optgroup option output
-  p param pre progress q rp rt ruby s samp script section select small source
-  span strong style sub summary sup table tbody td textarea tfoot th thead time
-  title tr track u ul var video wbr circle g line path polygon polyline rect svg
-  text)
+    (satisfies? IKVReduce obj)
+    (reduce-kv (fn [o k v] (gobj/set o (name k) v) o) #js{} obj)
+
+    (map? obj)
+    (reduce (fn [o [k v]] (gobj/set o (name k) v) o) #js{} obj)
+
+    :else obj))
+
+#_(dm/define-tag-functions)

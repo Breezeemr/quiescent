f9380d08067ad422b18f861bee2a27aa8769fd9b
ttallman <dspiteself@gmail.com>
2016-10-25 15:58:43 -0500

    bump quiescent version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject breeze-quiescent "0.1.7-SNAPSHOT"
+(defproject breeze-quiescent "0.1.8-SNAPSHOT"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}

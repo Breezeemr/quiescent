a0c0089cb56d0bbf6db25ea01713b07ee05ad2de
dan sutton <danielsutton01@gmail.com>
2019-02-12 18:15:17 -0600

    Add figwheel and index page so you can cljs repl now

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.breezeehr/quiescent "0.2.0-SNAPSHOT"
   :description "A minimal, functional ClojureScript wrapper for ReactJS"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
                  ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
@@ -20,19 +20,23 @@
 
   ;; development concerns
   :profiles {:dev {:dependencies
-                   [[org.clojure/clojure "1.7.0"]
-                    [org.clojure/clojurescript "1.7.145"]]
-                   :source-paths ["src" "examples/src"]
-                   :resource-paths ["examples/resources"]
-                   :plugins [[lein-cljsbuild "1.1.0"]]
+                   [[org.clojure/clojure "1.10.0"]
+                    [org.clojure/clojurescript "1.10.516"]
+                    [figwheel-sidecar "0.5.18"
+                     :exclusions [org.clojure/clojure cljsjs/react org.clojure/clojurescript]]
+                    [nrepl/nrepl "0.6.0"]]
+                   ;; :source-paths     ["src"]
+                   #_#_:resource-paths ["examples/resources"]
+                   :plugins            [[lein-cljsbuild "1.1.0"]
+                                        [lein-figwheel "0.5.18"]]
                    :cljsbuild
                    {:builds
                     [{:source-paths ["src" "examples/src"]
+                      :id           "dev-cljs"
+                      :figwheel     true
                       :compiler
-                      {:output-to "examples/resources/public/main.js"
-                       :output-dir "examples/resources/public/build"
-                       :optimizations :whitespace
-                       :pretty-print true
-                       :source-map
-                       "examples/resources/public/main.js.map"
+                      {:output-to        "resources/public/js/main.js"
+                       :output-dir       "resources/public/js/out"
+                       :main             "breeze.quiescent"
+                       :asset-path       "js/out"
                        :closure-warnings {:non-standard-jsdoc :off}}}]}}})
